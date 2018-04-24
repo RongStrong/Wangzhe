0f92dd25c7ba09166d21ded0c65dd2542c4a4e0e
Your Name <you@example.com>
2018-04-24 16:27:33 -0700

    m1

--- CPU/test.cpp
+++ CPU/test.cpp
@@ -5,13 +5,18 @@ using namespace std;
 
 int main(){
 	CpuOp cpuTester;
+    cout<< "readOverhead:" << cpuTester.readOverhead() << endl;
+	cout<< "loopOverhead:" << cpuTester.loopOverhead() << endl;
 	cout<< cpuTester.CreateProOverhead() << endl;
     vector<double> procOverhead = cpuTester.procedureOverhead();
-	cout<< "loopOverhead:"  << endl;
+	cout<< "procedureOverhead:"  << endl;
 	for(auto it = procOverhead.begin();it != procOverhead.end();it++){
 		cout<< *it << endl;
 	}
-	
-	cout<< "systemCallOverhead:" << cpuTester.systemCallOverhead() << endl;
+    cout<< "systemCallOverhead:" << cpuTester.systemCallOverhead() << endl;
+	cout<< "ConWitchProOverhead:" << cpuTester.ConWitchProOverhead() << endl;
+    cout<< "ConWitchThrOverhead:" << cpuTester.ConWitchThrOverhead() << endl;
+    cout<< "CreateProOverhead:" << cpuTester.CreateProOverhead() << endl;
+    cout<< "CreateThrOverhead:" << cpuTester.CreateThrOverhead() << endl;
 	return 0;
 }
