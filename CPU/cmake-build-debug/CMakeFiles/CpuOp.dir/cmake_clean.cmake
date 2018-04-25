4c7410e32b69395c96029bf622272225fd86170a
dingzhiyuan32 <dingzhiyuan@MBP-2.local>
2018-04-24 22:00:41 -0700

    cmakelists

---
+++ CPU/cmake-build-debug/CMakeFiles/CpuOp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CpuOp.dir/CpuOp.cpp.o"
+  "CMakeFiles/CpuOp.dir/test.cpp.o"
+  "CpuOp.pdb"
+  "CpuOp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CpuOp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
