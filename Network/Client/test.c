c6e52cfad0f99ff6cfef36d880f0435a3883dab3
RongStrong <rongstrongcarry@gmail.com>
2018-06-07 20:20:01 -0700

    final

--- Network/Client/test.c
+++ Network/Client/test.c
@@ -4,6 +4,6 @@ using namespace std;
 void startupClient(int op, int echoServPort, char *servIP);
 
 int main() {
-	startupClient(2, 8080, "127.0.0.1");
+	startupClient(2, 8080, "169.228.184.115");
 	return 0;
 }
